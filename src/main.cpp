0bc8f8e3ae6884a82f4d0d39a9985931f376517d
Vidhi Sahay <vidhisahay25@gmail.com>
2026-03-10 19:39:28 +0530

    Add real-time vital sign monitoring with continuous sensor simulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 // Heart rate generator
 int generateHeartRate() {
-
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(60, 120);
-
     return dist(gen);
 }
 
 // Oxygen level generator
 int generateOxygenLevel() {
-
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(90, 100);
-
     return dist(gen);
 }
 
 // Temperature generator
 float generateTemperature() {
-
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<> dist(36.0, 38.5);
-
     return dist(gen);
 }
 
@@ -37,13 +33,18 @@ int main() {
 
     cout << "Patient Monitoring System Running\n\n";
 
-    int heartRate = generateHeartRate();
-    int oxygen = generateOxygenLevel();
-    float temperature = generateTemperature();
+    while(true) {
 
-    cout << "Heart Rate: " << heartRate << " bpm\n";
-    cout << "Oxygen Level: " << oxygen << " %\n";
-    cout << "Temperature: " << temperature << " C\n";
+        int heartRate = generateHeartRate();
+        int oxygen = generateOxygenLevel();
+        float temperature = generateTemperature();
+
+        cout << "Heart Rate: " << heartRate << " bpm\n";
+        cout << "Oxygen Level: " << oxygen << " %\n";
+        cout << "Temperature: " << temperature << " C\n\n";
+
+        this_thread::sleep_for(chrono::seconds(1));
+    }
 
     return 0;
 }
